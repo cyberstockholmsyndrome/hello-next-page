e124ba2df3963578c5f79ffd89976c297d59ab93
boucherymusic <voignamirthecat@gmail.com>
2019-06-05 22:30:44 +0200

    mobile header size change

--- components/Title.js
+++ components/Title.js
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 
-const Heading = styled.h1`
-  font-size: 6em;
-  text-align: center;
+const StyledWrapper = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  align-items: center;
 `;
 
-const Title = props => <Heading>{props.title}</Heading>;
+const Heading = styled.h1`
+  font-size: 4em;
+  display: inline-block;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 6em;
+  }
+`;
+
+const Title = props => (
+  <StyledWrapper>
+    <Heading>{props.title}</Heading>
+  </StyledWrapper>
+);
 
 export default Title;
